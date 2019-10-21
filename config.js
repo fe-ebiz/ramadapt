43972ae9af6e62eb966e6972a4790310034e9457
vxggangxv <vxggangxv@naver.com>
2019-10-21 18:02:04 +0900

    update gulpfile & footer pc

--- config.js
+++ config.js
@@ -2,13 +2,11 @@ module.exports = function() {
 
 	var source       = 'src',
 		development  = 'dist',
+		test 		 = 'test',
 		remove       = ['.sass-cache', 'dist'],
 
 		// 템플릿 경로
 		template = {
-//			src  : source + '/jade/**/!(_)*.jade',
-//			parts: source + '/jade/**/_*.jade',
-//			dest : development + '/views'
 			src  : source + '/template/**/!(_)*.html',
 			parts: source + '/template/**/_*.html',
 			dest : development + '/views',
@@ -23,13 +21,13 @@ module.exports = function() {
 			dest_jp : development + '/views_jp',
 			src_m_jp  : source + '/template_m_jp/**/!(_)*.html',
 			parts_m_jp : source + '/template_m_jp/**/_*.html',
-			dest_m_jp : development + '/views_m_jp'
+			dest_m_jp : development + '/views_m_jp',
 		},
 
 		// Sass 경로
 		sass = {
-			src       : source + '/sass/**/*.{scss,sass}',
-			compassSrc: source + '/sass',
+			src: source + '/sass/**/!(_)*.{scss,sass}',
+			parts: source + '/sass/**/_*.{scss,sass}',
 			dest      : development + '/static/css'
 		},
 
@@ -45,21 +43,38 @@ module.exports = function() {
 			dest: development + '/static/js'
 		},
 
+		// Img 경로
+		img = {
+			// src : source + '/assets/img/**/*.{gif,jpg,png,ico}',
+			src: source + '/img/**/!(sprite)*/*',
+			src_sprite: source + '/img/**/sprite*/*',
+			dest: development + '/img',
+		},
+
+		// etc 경로
+		etc = {
+			src: source + '/!(css|scss|js|img)/**',
+			dest: development + '/static',
+		},
+
 		// HTML 옵션
 		htmlbeautify = {
 			"indentSize": 4
 		};
 
 	return {
-		del  : remove,
-		src  : source,
-		dev  : development,
-		
-		template : template,
-		css  : css,
-		sass : sass,
-		js   : js,
+		del: remove,
+		src: source,
+		test: test,
+		dev: development,
 
-		htmlbeautify : htmlbeautify
+		template: template,
+		css: css,
+		sass: sass,
+		js: js,
+		img: img,
+		etc: etc,
+
+		htmlbeautify: htmlbeautify
 	};
 };
